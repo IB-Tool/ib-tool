2688a38de57007dd2cbe557abe8e72826062c7c3
Stefano Della Chiesa <stefano.dellachiesa@gmail.com>
2024-01-19 09:58:07 +0100

    added copyright notic at code begin

--- Error_Classification.py
+++ Error_Classification.py
@@ -1,3 +1,29 @@
+#----------------------------------------------------------------------
+# Name:        IB-Tool2
+# Purpose:     Toolset for the delineation of settlements on the basis
+#              building footprints
+#
+# Author:      Oliver Harig
+#
+# Created:     10.05.2021
+# Cite:        Oliver Harig (2021). Toolset for the delineation of settlements on the basis building footprints, road network and land use data (v1.0) https://doi.org/10.26084/IOERFDZ-SOFT-001    
+# Licence:     MIT License
+               Copyright 2021 Oliver Harig
+#
+#              Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+#              associated documentation files (the "Software"), to deal in the Software without restriction, including
+#              without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#              copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+#              following conditions:
+#
+#              The above copyright notice, and this permission notice must be included in all copies or substantial portions of the Software.
+#
+#              THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+#              LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#              IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#              WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#              SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#----------------------------------------------------------------------
 
 # ------ IMPORTS ------
 
